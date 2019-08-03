96ec06c36dfc6d12412e49b802404c055f8d565e
balakirankumar <balakirankumar999@gmail.com>
2019-08-03 06:20:34 +0530

    small change

---
+++ simplecalculation.c
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1,num2;
+    cout<<"enter the first number : "<<endl;
+    cin>>num1;
+    cout<<"enter the second number : "<<endl;
+    cin >>num2;
+    cout<<"the sum of values is : "<<num1+num2;
+
+    return 0;
+}
