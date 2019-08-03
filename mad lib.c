ceef4efb5d2f8838aad04f5daf0df7e4e705a555
balakirankumar <balakirankumar999@gmail.com>
2019-08-03 06:36:53 +0530

    madlibgmae

---
+++ mad lib.c
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string colour,dish,name;
+    cout<<"enter the colour : ";
+    cin>>(cin,colour);
+    cout<<"enter the dish : ";
+    cin>>(cin,dish);
+    cout<<"enter your bestie's name : ";
+    cin>>(cin,name);
+    cout<<"Youour favorite colour is "<<colour<<endl;
+    cout<<"Your favorite dish is "<<dish<<endl;
+    cout<<"You love "<<name<<endl;
+    return 0;
+    
+}
